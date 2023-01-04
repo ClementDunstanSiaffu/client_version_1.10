d3cccde43b952d3c7956e012953462af94d969a9
ClementDunstanSiaffu <clementsiaffu95@gmail.com>
2023-01-04 16:18:35 +0300

    fixing translations

--- dist/widgets/advanced_selection_table_widget/dist/runtime/translations/en.js
+++ dist/widgets/advanced_selection_table_widget/dist/runtime/translations/en.js
@@ -1,5 +1,5 @@
 
-define( {
+System.register([], function (_export) {return {execute: function () {_export({
     _widgetLabel:"Advanced selection",
     "selectGeometry":"Select Geometry",
     "layers":"Layers",
@@ -26,4 +26,34 @@ define( {
     "fields":"Fields",
     "selectionAction":"Selection Action",
     "mapLoading":"Waiting map to load..."
-});
+})}}});
+
+
+// define( {
+//     _widgetLabel:"Advanced selection",
+//     "selectGeometry":"Select Geometry",
+//     "layers":"Layers",
+//     "zoomA":"Zoom A",
+//     "exportCsv":"Export in file csv",
+//     "exportJson":"Export to feature collection",
+//     "statistics":"Statistics",
+//     "createLayer":"Create Layer",
+//     "attributeTable":"View in attribute table",
+//     "delete":"Delete",
+//     "countOfItems":"Count of items",
+//     "sumOfValues":"Sum of Values",
+//     "minimum":"Minimum",
+//     "maximum":"Maximum",
+//     "average":"Average",
+//     "selectByPoint":"Selection by point",
+//     "selectionByLine":"Selection by line",
+//     "selectionByPolygon":"Selection by polygon",
+//     "selectionByRectangular":"Selection by rectangular",
+//     "selectionByCircle":"Selection by circle",
+//     "refresh":"Refresh",
+//     "ok":"OK",
+//     "cancel":"Cancel",
+//     "fields":"Fields",
+//     "selectionAction":"Selection Action",
+//     "mapLoading":"Waiting map to load..."
+// });
